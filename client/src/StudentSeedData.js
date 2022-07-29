3afdc1dc90b0019a2aebd6a713ba93cb2ed1f185
cdurham-dcsd <cdurham@dcsdk12.org>
2022-07-29 11:47:24 -0600

    EC-589 #comment add more seed data, reconfigure dashboard.jsx. #time 1h

--- client/src/StudentSeedData.js
+++ client/src/StudentSeedData.js
@@ -1,13 +1,34 @@
-const text = {
-    studentInfo: [
-        {
-            firstName: "Buddy",
-            lastName: "Wilson",
-            grade: "5",
-            schoolName: "Big Sky Elementary",
-            studentNumber: "123456",
-            daysAbsent: "0"
-        }
-    ]
-};
-export default text;
+ export const students = [
+    {
+        firstName: "Buddy",
+        lastName: "Wilson",
+        grade: "5",
+        schoolName: "Big Sky Elementary",
+        studentNumber: "958377",
+        daysAbsent: "0"
+    },
+    {
+        firstName: "Andy",
+        lastName: "Taylor",
+        grade: "2",
+        schoolName: "Capstone Middle School",
+        studentNumber: "123456",
+        daysAbsent: "0"
+    },
+    {
+        firstName: "Christiane",
+        lastName: "Mammoth",
+        grade: "9",
+        schoolName: "Vista Ridge High School",
+        studentNumber: "123456",
+        daysAbsent: "3"
+    },
+    {
+        firstName: "Connor",
+        lastName: "Mammoth",
+        grade: "7",
+        schoolName: "Vista Ridge High School",
+        studentNumber: "123456",
+        daysAbsent: "0"
+    }
+];
